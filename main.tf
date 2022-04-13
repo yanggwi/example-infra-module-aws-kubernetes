c8632d0506df741796de444bc4f95bead0045223
yanggwi <kingearlast@gmail.com>
2022-04-13 23:31:31 +0900

    작성중 에러난 상태

---
+++ main.tf
@@ -0,0 +1,150 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  cluster_name = "${var.cluster_name}-${var.env_name}"
+}
+
+# 클러스터 역할
+resource "aws_iam_role" "ms-cluster" {
+  name               = local.cluster_name
+  assume_role_policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "eks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  POLICY
+}
+
+# 클러스터 권한 정책
+resource "aws_iam_policy_attachment" "ms-cluster-AmazoneEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.ms-cluster.name
+}
+
+# 보안그룹
+resource "aws_security_group" "ms-cluster" {
+  name   = local.cluster_name
+  vpc_id = var.vpc_id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ms-up-running"
+  }
+}
+
+# 클러스터
+resource "aws_eks_cluster" "ms-up-running" {
+  name     = local.cluster_name
+  role_arn = aws_iam_role.ms-cluster.arn
+
+  vpc_config {
+    security_group_ids = [aws_security_group.ms-cluster.id]
+    subnet_ids         = var.cluster_subnet_ids
+  }
+
+  depends_on = [
+    aws_iam_policy_attachment.ms-cluster-AmazoneEKSClusterPolicy
+  ]
+}
+
+# 노드 역할
+resource "aws_iam_role" "ms-node" {
+  name = "${local.cluster_name}.node"
+
+  assume_role_policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  POLICY
+}
+
+# 노드 권한 정책 연결
+resource "aws_iam_role_policy_attachment" "ms-node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.ms-node.name
+}
+resource "aws_iam_role_policy_attachment" "ms-node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.ms-node.name
+}
+resource "aws_iam_role_policy_attachment" "ms-node-ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.ms-node.name
+}
+
+# EKS 노드 그룹 정의
+resource "aws_eks_node_group" "ms-node-group" {
+  cluster_name    = aws_eks_cluster.ms-up-running.name
+  node_group_name = "microservices"
+  node_role_arn   = aws_iam_role.ms-node.arn
+  subnet_ids      = var.nodegroup_subnet_ids
+
+  scaling_config {
+    desired_size = var.nodegroup_desired_size
+    max_size     = var.nodegroup_max_size
+    min_size     = var.nodegroup_min_size
+  }
+
+  disk_size      = var.nodegroup_disk_size
+  instance_types = var.nodegroup_instance_types
+
+  depends_on = [
+    aws_iam_role_policy_attachment.ms-node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.ms-node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.ms-node-ContainerRegistryReadOnly,
+  ]
+}
+
+resource "local_file" "kubeconfig" {
+  content  = <<KUBECONFIG_END
+
+    apiVersion: v1
+    clusters:
+    - cluster:
+      certificate-authority-data: ${aws_eks_cluster.ms-up-running.certificate_authority.0.data}
+      server: ${aws_eks_cluster.ms-up-running.endpoint}
+      name: ${aws_eks_cluster.ms-up-running.arn}
+    contexts:
+    - context:
+      cluster: ${aws_eks_cluster.ms-up-running.arn}
+      user: ${aws_eks_cluster.ms-up-running.arn}
+      name: ${aws_eks_cluster.ms-up-running.arn}
+    current-context: ${aws_eks_cluster.ms-up-running.arn}
+    kind: Config
+    preferences: {}
+    users:
+    - name: ${aws_eks_cluster.ms-up-running.arn}
+    user:
+      exec:
+        apiVersion: client.authentication.k8s.io/v1alpha1
+        command: aws-iam-authenticator
+        args:
+          - "token"
+          - "-i"
+          - "${aws_eks_cluster.ms-up-running.name}"
+  KUBECONFIG_END
+  filename = "kubeconfig"
+}
